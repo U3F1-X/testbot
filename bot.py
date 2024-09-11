e734181965876208a4b028e6487ed6835f3c09d4
U3F1-X <omar-u3f1@outlook.com>
2024-09-11 10:05:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telebot
 
 # ضع هنا توكن البوت الخاص بك
-API_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
+API_TOKEN = '7337226207:AAE64rtBTDSRb2kuTGOMNRcoLeYKg29A_bI'
 
 # إنشاء كائن البوت
 bot = telebot.TeleBot(API_TOKEN)
